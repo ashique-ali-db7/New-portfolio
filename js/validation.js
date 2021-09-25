13890a0149cc1accaecc169f6b3f192516651a26
ashique ali <ashiquealikmvkd@gmail.com>
2021-09-25 20:33:51 +0530

    second commit

--- js/validation.js
+++ js/validation.js
@@ -14,6 +14,36 @@ required:"This field is required"
   });
 });
 
+$("#submit-form").validate({
+  rules:{
+      name:{
+          required:true,
+          minlength:4,
+      },
+      email:{
+          required:true,
+          email:true
+      },
+      message:{
+          required:true,
+          minlength:10
+      }
+  },
+  submitHandler:function(form){
+      $.ajax({
+          url:"https://script.google.com/macros/s/AKfycbxTV1PmFUqev5rPq_2W-KIRSA5GlvusggjJ7M_o/exec",
+          data:$("#submit-form").serialize(),
+          method:"post",
+          success:function (response){
+              alert("Message submitted successfully")
+              window.location.reload()
+          },
+          error:function (err){
+              alert("Something Error")
+          }
+      })
+  }
+})
 
 
 
