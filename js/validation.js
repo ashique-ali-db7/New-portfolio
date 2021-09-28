11039c7db43c1844edbb4bcf566bb5876270082d
ashique ali <ashiquealikmvkd@gmail.com>
2021-09-28 13:06:33 +0530

    form submission

--- js/validation.js
+++ js/validation.js
@@ -10,7 +10,21 @@
     messages: {
 fname:"enter your full name",
 required:"This field is required"
-    }
+    },
+      submitHandler:function(form){
+      $.ajax({
+          url:"https://script.google.com/macros/s/AKfycbw1YAvn-K4C3UAGXVvvu6I7yydbtgFoGH-NEgNEpdOC6nXVH7P_QF3xHplr_ATtlK90/exec",
+          data:$("#signupform").serialize(),
+          method:"post",
+          success:function (response){
+              alert("Message submitted successfully")
+              window.location.reload()
+          },
+          error:function (err){
+              alert("Something Error")
+          }
+      })
+  }
   });
 
 
